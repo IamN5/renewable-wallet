821425109195da670aedb9f56ed95546954d92ca
Nicholas Segger <nicholas.segger5@gmail.com>
2023-07-07 18:27:49 -0300

    feat: Add initial theme and types

---
+++ src/utils/theme.ts
@@ -0,0 +1,18 @@
+const palette = {
+  primary: '#770FDF',
+  pressedPrimary: '#3E61DE',
+  subtitleGrey: '#6879B7',
+  disabledGrey: '#94A3DC',
+  grey: '#8F8F8F',
+  black: '#000000',
+  red: '#D65454',
+  green: '#528D63',
+  statusGreen: '#69B47E',
+  statusYellow: '#FFE792',
+  statusRed: '#FF6C6C',
+  messageGrey: '#E4E8F6',
+  tabBarBorder: 'rgba(148, 163, 220, 0.3)',
+  tabBarShadow: 'rgba(148, 163, 220, 1)',
+};
+
+export default palette;
