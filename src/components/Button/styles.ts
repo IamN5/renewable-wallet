2c769067e86fd18fe484295e42c004dbd85b2f79
Nicholas Segger <nicholas.segger5@gmail.com>
2023-07-09 01:38:14 -0300

    feat: Add Button component

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,27 @@
+import palette from '@utils/theme';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 4,
+    minWidth: 252,
+    height: 52,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: palette.primary,
+  },
+  label: {
+    lineHeight: 24,
+    color: 'white',
+    fontSize: 16,
+  },
+  labelWrapper: {
+    borderRadius: 20,
+    flex: 1,
+    paddingVertical: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default styles;
