91d00187384980a5f64c7c9d0cb201ba68221201
Nicholas Segger <nicholas.segger5@gmail.com>
2023-07-09 01:39:54 -0300

    fix: Adjust status bar color scheme

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { Appearance, StatusBar, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
@@ -7,6 +7,9 @@ import store from '@store/index';
 import RootNavigation from '@routes/index';
 
 function App(): JSX.Element {
+  Appearance.setColorScheme('light');
+  StatusBar.setBarStyle('dark-content');
+
   return (
     <SafeAreaProvider style={styles.container}>
       <NavigationContainer>
