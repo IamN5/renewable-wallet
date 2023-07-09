dca8fb6f9e97e0c1f46787711fa9d3ed877cfc5c
Nicholas Segger <nicholas.segger5@gmail.com>
2023-07-09 01:35:37 -0300

    refactor: Refactor user slice to use async thunk to login/logout

--- src/store/slices/user.ts
+++ src/store/slices/user.ts
@@ -1,5 +1,6 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { IUser, IUserLoginForm } from '@app-types/user';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { ACCESS_TOKEN_KEY, IUser, IUserLoginForm } from '@app-types/user';
+import storage from '@services/storage';
 
 interface IUserState {
   user: IUser;
@@ -19,6 +20,43 @@ const initialState: IUserState = {
   },
   isLoggedIn: false,
 };
+
+export const tryUserLogin = createAsyncThunk<IUser | null, IUserLoginForm>(
+  'user/tryUserLogin',
+  async ({ email }, { rejectWithValue }) => {
+    // Here we would try to login the user by POSTing our API
+
+    try {
+      await storage.set(ACCESS_TOKEN_KEY, 'dummy_token');
+
+      return {
+        email,
+        firstName: 'John',
+        lastName: 'Doe',
+        balance: 161100,
+        totalPortfolio: 517770,
+        portfolioRatio: 0.2305,
+      };
+    } catch (e) {
+      console.error('Error while cacheing user token: ', e);
+      // TODO: Properly handle and display error
+      // return rejectWithValue(e);
+    }
+
+    return null;
+  },
+);
+
+export const userLogout = createAsyncThunk<void, void>(
+  'user/userLogout',
+  async () => {
+    try {
+      await storage.remove(ACCESS_TOKEN_KEY);
+    } catch (e) {
+      console.error('Error while removing user token: ', e);
+    }
+  },
+);
 
 const userSlice = createSlice({
   name: 'user',
@@ -40,6 +78,25 @@ const userSlice = createSlice({
         },
       };
     },
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(tryUserLogin.fulfilled, (state, action) => {
+        if (!action.payload) {
+          return state;
+        }
+
+        return {
+          ...state,
+          isLoggedIn: true,
+          user: action.payload,
+        };
+      })
+      .addCase(userLogout.fulfilled, state => ({
+        ...state,
+        isLoggedIn: false,
+        user: initialState.user,
+      }));
   },
 });
 
