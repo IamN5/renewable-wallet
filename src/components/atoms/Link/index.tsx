8aa510e5f4730c46070745c704483e0d595d5e43
Nicholas Segger <nicholas.segger5@gmail.com>
2023-07-09 01:36:44 -0300

    feat: Add Link atom

---
+++ src/components/atoms/Link/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text } from 'react-native';
+import styles from './style';
+
+interface ILink {
+  children: string;
+  onPress: () => void;
+}
+
+function Link({ children, onPress }: ILink) {
+  return (
+    <Text onPress={onPress} style={styles.link}>
+      {children}
+    </Text>
+  );
+}
+
+export default Link;
